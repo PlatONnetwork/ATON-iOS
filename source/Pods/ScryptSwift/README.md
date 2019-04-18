fb701244f27d67efe624979b996beaba00267142
weixuening <weixuening@gmail.com>
2019-04-18 17:51:15 +0800

    remove pod directory,pod file specify version

--- source/Pods/ScryptSwift/README.md
+++
@@ -1,2 +0,0 @@
-# ScryptSwift
-Scrypt algorithm for swift
--- source/Pods/Target Support Files/Pods-platonWalletUITests/Pods-platonWalletUITests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_platonWalletUITests : NSObject
-@end
-@implementation PodsDummy_Pods_platonWalletUITests
-@end
