5009ec73a59b50159631ea1140b76b4709a29486
Ham Yu <yuhamgg@gmail.com>
2019-08-16 10:53:25 +0800

    add files

---
+++ source/platonWallet/Models/Wallet/Balance.swift
@@ -0,0 +1,15 @@
+//
+//  Balance.swift
+//  platonWallet
+//
+//  Created by Admin on 15/8/2019.
+//  Copyright © 2019 ju. All rights reserved.
+//
+
+import Foundation
+
+struct Balance: Decodable {
+    var addr: String
+    var free: String? //自由账户余额
+    var lock: String? //锁仓账户余额
+}
