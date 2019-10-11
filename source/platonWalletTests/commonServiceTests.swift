4637ea4bc5da67b1ea3ef628823ca3c3fadfb083
Ned <cocolee@platon.network>
2019-10-11 21:13:56 +0800

    unit test

--- source/platonWalletTests/commonServiceTests.swift
+++ source/platonWalletTests/commonServiceTests.swift
@@ -26,7 +26,7 @@ class CommonServicegTest: XCTestCase {
     }
     
     func testCommonService() {
-        CommonService.isValidContractAddress("test")
+        XCTAssert(!CommonService.isValidContractAddress("test").0)
         CommonService.isValidContractAddress("0x5eBb663FD101b46dBBe6465E72Ed4b2918492011")
         CommonService.isValidWalletName("test")
         CommonService.isValidWalletName("test", checkDuplicate: true)
@@ -52,5 +52,172 @@ class CommonServicegTest: XCTestCase {
         AnalysisHelper.handleEvent(id: "test", operation: EventOperation.begin, attributes: nil)
         AnalysisHelper.handleEvent(id: "test", operation: EventOperation.cancel, attributes: nil)
     }
+    
+    func testBaseService(){
+        let b = BaseService()
+        
+        var block : platonWallet.PlatonCommonCompletion? = nil
+        b.successCompletionOnMain(obj: nil, completion: &block)
+        b.failCompletionOnMainThread(code: -1, errorMsg: "", completion: &block)
+        b.timeOutCompletionOnMainThread(completion: &block)
+        b.failWithEmptyResponseCompletionOnMainThread(completion: &block)
+        
+        DispatchQueue.global().async {
+            b.successCompletionOnMain(obj: nil, completion: &block)
+            b.failCompletionOnMainThread(code: -1, errorMsg: "", completion: &block)
+            b.timeOutCompletionOnMainThread(completion: &block)
+            b.failWithEmptyResponseCompletionOnMainThread(completion: &block)
+        }
+        
+    }
+    
+    func testTransfer(){
+        TransactionService.service.sendAPTTransfer(from: "0x5eBb663FD101b46dBBe6465E72Ed4b2918492061", to: "0x5eBb663FD101b46dBBe6465E72Ed4b2918492061", amount: "1", InputGasPrice: BigUInt("1"), estimatedGas: "111", memo: "", pri: "c3ab73eb6b0e414f0d8949bd76cb58ab8fd5d348dbb24522bdac5da74b885bac") { (ret, obj) in
+            
+        }
+        
+        
+        TransactionService.service.startGasTimer()
+        TransactionService.service.stopGasTimer()
+        
+        let tx = Transaction()
+        tx.txhash = "0x1111111"
+        tx.transactionType = 0
+        TransferPersistence.add(tx: tx)
+    }
+    
+    func testWeb3Helper(){
+        let correcturl = "https:\\aton.main.platon.network"
+        let errorurl = "https:\\aton.error.platon.network"
+        Web3Helper.switchRpcURL(correcturl) { (result) in
+            
+        }
+        Web3Helper.switchRpcURL(correcturl, succeedCb: { 
+        }) { 
+        }
+        
+        Web3Helper.switchRpcURL(errorurl) { (result) in
+            
+        }
+        Web3Helper.switchRpcURL(errorurl, succeedCb: { 
+        }) { 
+        }
+    }
+    
+    func testByteHelper(){
+        let data = Data(hexStr: "0x0a0b")
+        data?.getByte(at: 0)
+        data?.getUnsignedLong(at: 0)
+        
+        Data.newData(uint32data: 1)
+    }
+    
+    func testAddressBookService(){
+        let addr = AddressInfo()
+        addr.nodeURLStr = "https://aton.main.platon.network"
+        addr.addressType = 0
+        addr.walletAddress = "0xbbbb"
+        addr.walletName = "name1"
+        AddressBookService.service.add(addressInfo: addr)
+        
+        addr.walletAddress = "0xaaa"
+        AddressBookService.service.replaceInto(addrInfo: addr)
+        
+        AddressBookService.service.getAll()
+    }
+    
+    func testTransactionQrcode(){
+        let code0 = TransactionQrcode(amount: "", chainId: "", from: "", to: "", gasLimit: "", gasPrice: "", nonce: "", typ: 0, nodeId: "", nodeName: "", sender: "", stakingBlockNum: "", type: 0)
+        let code1004 = TransactionQrcode(amount: "", chainId: "", from: "", to: "", gasLimit: "", gasPrice: "", nonce: "", typ: 0, nodeId: "", nodeName: "", sender: "", stakingBlockNum: "", type: 1004)
+        let code1005 = TransactionQrcode(amount: "", chainId: "", from: "", to: "", gasLimit: "", gasPrice: "", nonce: "", typ: 0, nodeId: "", nodeName: "", sender: "", stakingBlockNum: "", type: 1005)
+        
+        code0.typeString
+        code0.fromName
+        code0.toName
+        
+        code1004.typeString
+        code1004.fromName
+        code1004.toName
+        
+        code1005.typeString
+        code1005.fromName
+        code1005.toName
+    }
+    
+    func testNode(){
+        
+        let nodeActive = Node(nodeId: "test", ranking: 1, name: "name", deposit: "100", url: "random", ratePA: "1", nStatus: NodeStatus.Active, isInit: true)
+        let nodeCandidate = Node(nodeId: "test", ranking: 1, name: "name", deposit: "100", url: "random", ratePA: "1", nStatus: NodeStatus.Candidate, isInit: true)
+        let nodeExited = Node(nodeId: "test", ranking: 1, name: "name", deposit: "100", url: "random", ratePA: "1", nStatus: NodeStatus.Exited, isInit: true)
+        let nodeExiting = Node(nodeId: "test", ranking: 1, name: "name", deposit: "100", url: "random", ratePA: "1", nStatus: NodeStatus.Exiting, isInit: true)
+        
+        nodeActive.nStatus
+        nodeCandidate.nStatus
+        nodeExited.nStatus
+        nodeExiting.nStatus
+        
+        nodeActive.status
+        nodeCandidate.status
+        nodeExited.status
+        nodeExiting.status
+        
+        nodeActive.rank
+        nodeCandidate.rank
+        nodeExited.rank
+        nodeExiting.rank
+        
+    }
+    
+    func testTransaction(){
+        let tx = Transaction()
+        let typesEnum = [TxType.transfer,
+                         TxType.contractCreate,
+                         TxType.contractExecute,
+                         TxType.otherReceive,
+                         TxType.otherSend,
+                         TxType.MPCtransaction,
+                         TxType.stakingCreate,
+                         TxType.stakingEdit,
+                         TxType.stakingAdd,
+                         TxType.stakingWithdraw,
+                         TxType.delegateCreate,
+                         TxType.delegateWithdraw,
+                         TxType.submitText,
+                         TxType.submitVersion,
+                         TxType.submitParam,
+                         TxType.voteForProposal,
+                         TxType.declareVersion,
+                         TxType.submitCancel,
+                         TxType.reportDuplicateSign,
+                         TxType.createRestrictingPlan,
+                         TxType.unknown]
+        
+        
+        for item in typesEnum{
+            tx.txType = item
+            tx.txType?.localizeTitle
+            
+            tx.toAvatarImage
+            tx.fromAvatarImage
+            tx.toNameString
+            tx.fromNameString
+            tx.toIconImage
+            tx.amountTextString
+            tx.amountTextColor
+            tx.txTypeIcon
+            tx.pipString
+            tx.versionDisplayString
+            tx.recordIconIV
+            tx.recordAmount
+            tx.recordAmountForDisplay
+            tx.recordStatus
+            tx.recordTime
+            tx.recordWalletName
+            
+            
+        }
+         
+    }
+    
 }
     
