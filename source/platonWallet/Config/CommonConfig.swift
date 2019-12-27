528b5948d547a0cefc50a9ccd0b20c369af1ca39
Ham <yuhamgg@gmail.com>
2019-12-27 10:12:50 +0800

    add extranet ip

--- source/platonWallet/Config/CommonConfig.swift
+++ source/platonWallet/Config/CommonConfig.swift
@@ -103,7 +103,8 @@ struct AppConfig {
         struct HOST {
             static let UATNET = "https://aton.test.platon.network"
             static let PRODUCTNET = "https://aton.main.platon.network"
-            static let TESTNET = "http://192.168.9.190:1000"
+//            static let TESTNET = "http://192.168.9.190:1000"
+            static let TESTNET = "http://58.250.250.234:1000"
 //            static let TESTNET = "http://58.250.250.234:1000"
             static let DEVNET = "http://192.168.9.190:443"
         }
