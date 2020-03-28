d1114f8b4b8869b2b2299ad3b7d63c637a153eea
Ham <yuhamgg@gmail.com>
2020-03-28 10:37:04 +0800

    change channelcode

--- source/platonWallet/Config/RemoteServices.swift
+++ source/platonWallet/Config/RemoteServices.swift
@@ -18,7 +18,7 @@ class RemoteServices {
         var parameters: Parameters = [:]
         parameters["versionCode"] = buildVersion
         parameters["deviceType"] = "ios"
-        parameters["channelCode"] = "Testflight"
+        parameters["channelCode"] = "AppStore" // AppStore TestFlight
         NetworkService.request("/config/checkUpdate", parameters: parameters, completion: completion)
     }
 
