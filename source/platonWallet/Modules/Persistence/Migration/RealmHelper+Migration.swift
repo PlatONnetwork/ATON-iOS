02b7f29b9e170b4180f7dd33428b07f4500a4789
liyf <liyanfeng@juzxi.net>
2020-04-22 14:05:58 +0800

    提高链id到102

--- source/platonWallet/Modules/Persistence/Migration/RealmHelper+Migration.swift
+++ source/platonWallet/Modules/Persistence/Migration/RealmHelper+Migration.swift
@@ -15,6 +15,22 @@ extension RealmHelper {
         migration.renameProperty(onType: Node.className(), from: "ratePA", to: "delegatedRatePA")
     }
 
+    public static func migrationBelow0120(migration: Migration, schemaVersion:UInt64, oldSchemaVersion: UInt64) {
+        #if UAT
+        #elseif PARALLELNET
+        #else
+        migration.enumerateObjects(ofType: Wallet.className()) { (oldObject, newObject) in
+            guard oldObject != nil, newObject != nil else { return }
+            guard let chainId = oldObject!["chainId"] as? String, (chainId == AppConfig.ChainID.VERSION_074 
+                    || chainId == AppConfig.ChainID.VERSION_0741
+                    || chainId == AppConfig.ChainID.VERSION_076
+                    || chainId == AppConfig.ChainID.VERSION_0110
+            ) else { return }
+            newObject!["chainId"] = AppConfig.ChainID.VERSION_MAINTESTNET
+        }
+        #endif
+    }
+
     public static func migrationBelow091(migration: Migration, schemaVersion:UInt64, oldSchemaVersion: UInt64) {
         migration.enumerateObjects(ofType: Wallet.className()) { (oldObject, newObject) in
             guard oldObject != nil, newObject != nil else { return }
